47ac44a8a8bc454cf6487d8cb37423cfbcafedb8
Cody Weber <codyaweber@gmail.com>
2019-10-15 10:31:52 -0400

    Updated playground

--- Contents.swift
+++ Contents.swift
@@ -15,6 +15,7 @@ class MyViewController : UIViewController {
         
         addSegmentedControl()
         addButton()
+        addButtonTwo()
     }
     
     private func addSegmentedControl() {
@@ -23,8 +24,9 @@ class MyViewController : UIViewController {
         segmentedControl.insertSegment(withTitle: "One", at: 0, animated: true)
         segmentedControl.insertSegment(withTitle: "Two", at: 1, animated: true)
         
+        segmentedControl.selectedSegmentIndex = 0
         segmentedControl.backgroundColor = .lightGray
-//        segmentedControl.selectedSegmentTintColor = .orange
+        segmentedControl.selectedSegmentTintColor = .orange
         view.addSubview(segmentedControl)
         
         self.segmentedControl = segmentedControl
@@ -44,8 +46,27 @@ class MyViewController : UIViewController {
         self.button = button
     }
     
+    private func addButtonTwo() {
+        let frame = CGRect(x: 50, y: 200, width: 280, height: 40)
+        let button = UIButton(type: .system)
+        button.frame = frame
+        
+        button.setTitle("Set No Segment & setNeedsLayout", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .lightGray
+        button.addTarget(self, action: #selector(setNoSegmentAndSetNeedsLayout), for: .touchUpInside)
+        view.addSubview(button)
+        
+        self.button = button
+    }
+    
     @objc func setNoSegment() {
         self.segmentedControl?.selectedSegmentIndex = UISegmentedControl.noSegment
+    }
+    
+    @objc func setNoSegmentAndSetNeedsLayout() {
+        self.segmentedControl?.selectedSegmentIndex = UISegmentedControl.noSegment
+        self.segmentedControl?.setNeedsLayout()
     }
 }
 // Present the view controller in the Live View window
